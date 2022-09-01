75b0035903b4d71d3c04c73d26710acee305b052
Duran11 <antonio11duran@gmail.com>
2022-09-01 08:24:32 -0600

    Fixed generate password function to js

--- assets/script.js
+++ assets/script.js
@@ -3,26 +3,41 @@ var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
-var password = document.getElementById("password)");
 
-var passwordLength = function () {
-    var charLength = parseInt(prompt("Please choose a password length between 8-128 characters:"));
+var charLength = "";
+var upperQuestion;
+var numberQuestion;
+var specialQuestion;
 
-    if (charLength < 8 || charLength > 128 || charLength === " ") {
-        alert("Please choose a password length between 8-128.");
-        return passwordLength;
-    } else {
-        alert("You chose a length of " + charLength)
-    }
-}
 function generatePassword() {
-    passwordLength();
-    var password = "";
-    var charChoice = lowercase;
-    var upperQuestion = confirm("Would you like uppercase letters?");
-    var numberQuestion = confirm("Would you like numbers?");
-    var specialQuestion = confirm("Would you like special characters?");
+        var charLength = parseInt(prompt("Please choose a password length between 8-128 characters:"));
+
+        if (charLength < 8 || charLength > 128 || charLength === " ") {
+            alert("Please choose a password length between 8-128.");
+            var charLength = parseInt(prompt("Please choose a password length between 8-128 characters:"));
+        } else {
+            alert("You chose a length of " + charLength);
+        }
+
+    var lowercaseQuestion = confirm("Would you like to include lowercase letters?");
+    var upperQuestion = confirm("Would you like to include uppercase letters?");
+    var numberQuestion = confirm("Would you like to inlcude numbers?");
+    var specialQuestion = confirm("Would you like special to include characters?");
     
+    if (!lowercaseQuestion && !upperQuestion && !numberQuestion && !specialQuestion) {
+        alert("Please select one of the parameters.");
+        var lowercaseQuestion = confirm("Would you like to include lowercase letters?");
+        var upperQuestion = confirm("Would you like to include uppercase letters?");
+        var numberQuestion = confirm("Would you like to inlcude numbers?");
+        var specialQuestion = confirm("Would you like special to include characters?");
+    }
+
+    var charChoice = [];
+    
+    if (lowercaseQuestion) {
+        charChoice = charChoice.concat(lowercase)
+    };
+
     if (upperQuestion) {
         charChoice = charChoice.concat(uppercase)
     };
@@ -35,11 +50,15 @@ function generatePassword() {
         charChoice = charChoice.concat(special)
     };
 
-    for (var i = 0; i < passwordLength.charLength; i++) {
-        var randomNumber = Math.floor(Math.random() * charChoice.length);
-        password += charChoice.substring(randomNumber, randomNumber + 1);
+    console.log(charChoice);
+
+    var retVal = "";
+
+    for (var i = 0; i < charLength; i++) {
+        retVal = retVal + charChoice[Math.floor(Math.random() * charChoice.length)];
+        console.log(retVal);
     };
-    document.getElementById("password").value = password;
+    return retVal;
 }
 
 var generateBtn = document.querySelector("#generate");
